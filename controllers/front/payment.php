05bf0368023c88469678186081de32a7d25e94b4
Fabien Serny <fabien@23prod.fr>
2016-09-08 15:41:41 +0200

    Fix return URL

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -44,7 +44,7 @@ class OystPaymentModuleFrontController extends ModuleFrontController
             'notification' => $this->context->link->getModuleLink('oyst', 'paymentNotification').$glue.'key='.Configuration::get('FC_OYST_HASH_KEY').'&ch='.$cart_hash,
             'cancel' => $this->context->link->getModuleLink('oyst', 'paymentError'),
             'error' => $this->context->link->getModuleLink('oyst', 'paymentError'),
-            'return' => $this->context->link->getPageLink('order'),
+            'return' => $this->context->link->getPageLink('order-confirmation').$glue.'id_cart='.$this->context->cart->id.'&id_module='.Module::getModuleIdByName('oyst').'&key='.$this->context->customer->secure_key,
         );
         $currency = new CurrencyCore($this->context->cart->id_currency);
         $total_amount = (int)ceil($this->context->cart->getOrderTotal() * 100);
