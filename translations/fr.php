b320c8e924d2e575deec4d811b2a00b43f016ff4
Fabien Serny <fabien@23prod.fr>
2016-09-08 11:05:31 +0200

    Add french translations

---
+++ translations/fr.php
@@ -0,0 +1,27 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{oyst}prestashop>oyst_4952647bd504f1369bde6167a667609b'] = 'Oyst';
+$_MODULE['<{oyst}prestashop>error.bootstrap_19646c9b73b696aac0fba419ccc641fc'] = 'Une erreur est survenue avec le système de paiement Oyst. Veuillez contacter le support.';
+$_MODULE['<{oyst}prestashop>displaypayment.bootstrap_e77bb171c7ae55bba8b2ed869fadfb19'] = 'Payer par carte bancaire';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_4725c2b6db34f755b89b741c1d45bc71'] = 'La nouvelle configuration a été sauvegardée';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_9b0c85b97abeed9539a4a27522f07b86'] = 'Vous devez activer l\'option \"allow_url_fopen\" sur votre serveur pour utiliser ce module !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_1e9a47f645f877ccb0802c12a409fcdf'] = 'Vous devez activer l\'extension \"cURL\" sur votre serveur pour utiliser ce module !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_fbfb2fca2198f2de73351ede1f2e7058'] = 'Configuration du module Oyst';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_4952647bd504f1369bde6167a667609b'] = 'Oyst';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_f2d5f048faab02254c8ee0de7e65d66a'] = 'Activer le système paiement';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_7ab39793f729d27b9fdf353192d3065a'] = 'Activez la fonctionnalité de paiement !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_74b95e43c22a19e49968aca2185a9c7d'] = 'Export catalogue';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_55864aa19ad755ef5f01d1e89b77a1b1'] = 'Exportez votre catalogue vers Oyst pour augmenter votre nombre de commandes !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_669cb2eb32a6f005821f6b78d1079bb0'] = 'Importation des commandes';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_e7564566c2d558efdd7d1c17fa812b82'] = 'Importer les commandes de vos produits effectués sur Oyst !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_32457e80793e343086c3369850877938'] = 'Veuillez remplir votre clé API Oyst :';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_7efe355afadb7fc768cc54174a0fec62'] = 'Vous avez besoin de cette clé pour utiliser le système de paiement Oyst mais également pour exporter votre catalogue et importer vos commandes.';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_1e755a12eb9bf76c24d7a372a294b1d6'] = 'Votre clé API est valide !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_8166540c7fd52a23da06af9c9ea1b0ec'] = 'Votre clé API semble invalide !';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_e878296944f4e1f0fe27c3ba0558a215'] = 'Endpoint paiement Oyst :';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_fded9ddc991365bc702700dacbba1cab'] = 'Si vous n\'êtes pas sûr de ce qu\'il faut mettre, ne changez rien.';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_66a1677105157b0a86df0f266bb49774'] = 'Endpoint catalogue Oyst :';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_28de547ec268875268041b5765f820ca'] = 'Url de notification';
+$_MODULE['<{oyst}prestashop>getcontent.bootstrap_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
