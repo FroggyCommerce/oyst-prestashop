b7ba5d02926702ca217c7139238045493dcbe159
Fabien Serny <fabien@23prod.fr>
2016-09-09 23:51:30 +0200

    Add Uptain tracker

---
+++ hooks/OystHookDisplayHeaderProcessor.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 2013-2016 Froggy Commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * You should have received a licence with this module.
+ * If you didn't download this module on Froggy-Commerce.com, ThemeForest.net,
+ * Addons.PrestaShop.com, or Oyst.com, please contact us immediately : contact@froggy-commerce.com
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to benefit the updates
+ * for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Froggy Commerce <contact@froggy-commerce.com>
+ * @copyright 2013-2016 Froggy Commerce / 23Prod / Oyst
+ * @license GNU GENERAL PUBLIC LICENSE
+ */
+
+/*
+ * Security
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class OystHookDisplayHeaderProcessor extends FroggyHookProcessor
+{
+    public function run()
+    {
+        return $this->module->fcdisplay(__FILE__, 'displayHeader.tpl');
+    }
+}
