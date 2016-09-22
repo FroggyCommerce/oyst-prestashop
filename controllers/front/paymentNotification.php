533d70c1bd88343e775199f355b18969fb2fb8ab
Fabien Serny <fabien@23prod.fr>
2016-09-22 16:20:34 +0200

    Orders validated with Oyst now indicate "CB" as payment method

--- controllers/front/paymentNotification.php
+++ controllers/front/paymentNotification.php
@@ -124,6 +124,6 @@ class OystPaymentNotificationModuleFrontController extends ModuleFrontController
         }
 
         // Validate order
-        $this->module->validateOrder($cart->id, $payment_status, $transaction['total_paid'], $this->module->displayName, $message, $transaction, $cart->id_currency, false, $this->context->customer->secure_key, $shop);
+        $this->module->validateOrder($cart->id, $payment_status, $transaction['total_paid'], 'CB', $message, $transaction, $cart->id_currency, false, $this->context->customer->secure_key, $shop);
     }
 }
