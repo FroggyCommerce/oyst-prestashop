e642e76f013e5e77285122dfebd3bfc563c4e772
Fabien Serny <fabien@23prod.com>
2016-12-19 15:11:45 +0100

    Add information message about cancellation on order pages paid with Oyst

---
+++ hooks/OystHookDisplayAdminOrderProcessor.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 2013-2016 Froggy Commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * You should have received a licence with this module.
+ * If you didn't download this module on Froggy-Commerce.com, ThemeForest.net,
+ * Addons.PrestaShop.com, or Oyst.com, please contact us immediately : contact@froggy-commerce.com
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to benefit the updates
+ * for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Froggy Commerce <contact@froggy-commerce.com>
+ * @copyright 2013-2016 Froggy Commerce / 23Prod / Oyst
+ * @license   GNU GENERAL PUBLIC LICENSE
+ */
+
+/*
+ * Security
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class OystHookDisplayAdminOrderProcessor extends FroggyHookProcessor
+{
+    public function run()
+    {
+        // Check if order has been paid with Oyst
+        $order = new Order(Tools::getValue('id_order'));
+        if ($order->module != 'oyst') {
+            return '';
+        }
+
+        $assign = array();
+        $assign['module_dir'] = $this->path;
+        $this->smarty->assign($this->module->name, $assign);
+
+        return $this->module->fcdisplay(__FILE__, 'displayAdminOrder.tpl');
+    }
+}
---
+++ views/templates/hook/displayAdminOrder.bootstrap.tpl
@@ -0,0 +1,23 @@
+{**
+ * 2013-2016 Froggy Commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * You should have received a licence with this module.
+ * If you didn't download this module on Froggy-Commerce.com, ThemeForest.net,
+ * Addons.PrestaShop.com, or Oyst.com, please contact us immediately : contact@froggy-commerce.com
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to benefit the updates
+ * for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Froggy Commerce <contact@froggy-commerce.com>
+ * @copyright 2013-2016 Froggy Commerce / 23Prod / Oyst
+ * @license GNU GENERAL PUBLIC LICENSE
+ *}
+
+<div class="alert alert-info">
+    L'annulation ou remboursement du paiement rattaché à cette commande ne sera pas pris en compte depuis votre Back Office Prestashop. Veuillez directement vous connecter à votre Back Office Oyst (https://pay.oyst.com) afin de gérer vos paiements
+</div>
